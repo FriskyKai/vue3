bbcdffed8ceae49aad896c297737e827941cf80c
FriskyKai <aptem5196@gmail.com>
2024-03-11 16:52:32 +0700

    Проект для тренировки Vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    data() {
+      return {
+        text: '',
+        words: []
+      }
+    },
+    methods: {
+      cut: function () {
+        this.words = this.text.split(' ')
+      }
+    }
+  }
+</script>
+
+<template>
+  <textarea v-model="text"></textarea>
+  <button @click="cut">Разрезать</button>
+  <ul>
+    <li v-for="word in words">{{ word }}</li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
